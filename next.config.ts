0c71889b2ff39b229ccf193e365101651d6bf4ba
sarthakwararkar <sarthakwararkar2@gmail.com>
2026-03-06 18:40:18 +0530

    fix(routing): use zero-config for Next.js+FastAPI to preserve POST method (remove vercel.json)

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* optional local config or Vercel config rules */
+  rewrites: async () => {
+    return [
+      {
+        source: "/api/:path*",
+        destination:
+          process.env.NODE_ENV === "development"
+            ? "http://127.0.0.1:8000/api/:path*"
+            : "/api/",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
